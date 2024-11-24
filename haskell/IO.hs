941cf6096eb6cda2c2e0110d33b555b7f9db66b2
criskell <cris.moraes.ds@gmail.com>
2024-11-24 00:22:37 -0300

    haskell: cli arguments

--- haskell/IO.hs
+++ haskell/IO.hs
@@ -1,27 +1,40 @@
-import System.IO
-import System.Directory
+-- import System.IO
+-- import System.Directory
+-- import Data.List
+
+-- main = do
+--     handle <- openFile "todos.txt" ReadMode
+--     (tempName, tempHandle) <- openTempFile "." "temp"
+    
+--     contents <- hGetContents handle
+--     let todoTasks = lines contents
+--         numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    
+--     putStrLn "These are your TO-DO items:"
+--     putStr $ unlines numberedTasks
+--     putStrLn "Which one do you want to delete?"
+--     numberString <- getLine
+
+--     let number = read numberString
+--         newTodoItems = delete (todoTasks !! number) todoTasks
+
+--     hPutStr tempHandle $ unlines newTodoItems
+    
+--     hClose handle
+--     hClose tempHandle
+
+--     removeFile "todos.txt"
+--     renameFile tempName "todos.txt"
+
+import System.Environment
 import Data.List
 
 main = do
-    handle <- openFile "todos.txt" ReadMode
-    (tempName, tempHandle) <- openTempFile "." "temp"
+    args <- getArgs
+    progName <- getProgName
     
-    contents <- hGetContents handle
-    let todoTasks = lines contents
-        numberedTasks = zipWith (\n line -> show n ++ " - " ++ line) [0..] todoTasks
+    putStrLn "The arguments are:"
+    mapM putStrLn args
     
-    putStrLn "These are your TO-DO items:"
-    putStr $ unlines numberedTasks
-    putStrLn "Which one do you want to delete?"
-    numberString <- getLine
-
-    let number = read numberString
-        newTodoItems = delete (todoTasks !! number) todoTasks
-
-    hPutStr tempHandle $ unlines newTodoItems
-    
-    hClose handle
-    hClose tempHandle
-
-    removeFile "todos.txt"
-    renameFile tempName "todos.txt"
+    putStrLn "The program name is:"
+    putStrLn progName
