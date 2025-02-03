6fdd9b687f3115223338c6d447cb7f9b18a5eeb3
criskell <cris.moraes.ds@gmail.com>
2025-02-02 21:01:30 -0300

    haskell: kinds, functors, type applications, forall

---
+++ haskell/topics/Inference-Kinds-Functors.hs
@@ -0,0 +1,17 @@
+{-# OPTIONS_GHC -fno-warn-partial-type-signatures #-}
+{-# LANGUAGE TypeApplications, ExplicitForAll, PartialTypeSignatures #-}
+
+import Data.Kind (Type)
+
+infixr |>
+(|>) :: forall (b :: Type). _
+(|>) = flip ((<$>) @((->) b))
+
+main :: IO ()
+main = do
+  let resultadoEventual = (+100)
+      modificadorDesseResultadoEventual = (subtract 100)
+      
+  putStrLn . show . resultadoEventual |> modificadorDesseResultadoEventual $ 1000
+  putStrLn . show . modificadorDesseResultadoEventual <$> resultadoEventual $ 1000
+  
