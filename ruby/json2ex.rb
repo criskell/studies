082c9056d35a2b9fe84600087bfb9a3640b8fbf4
criskell <cris.moraes.ds@gmail.com>
2025-11-21 06:28:35 -0300

    sync

--- ruby/json2ex.rb
+++ ruby/json2ex.rb
@@ -1,3 +1,5 @@
+#!/usr/bin/env ruby
+
 require "json"
 
 def to_elixir(value, atom_keys: false, indent: 0)
@@ -40,62 +42,14 @@ def to_elixir(value, atom_keys: false, indent: 0)
   end
 end
 
-json = <<~JSON
-{
-  "charge": {
-    "status": "ACTIVE",
-    "customer": {
-      "name": "Dan",
-      "email": "email0@example.com",
-      "phone": "5511999999999",
-      "taxID": {
-        "taxID": "31324227036",
-        "type": "BR:CPF"
-      }
-    },
-    "value": 100,
-    "comment": "good",
-    "correlationID": "9134e286-6f71-427a-bf00-241681624586",
-    "paymentLinkID": "7777a23s-6f71-427a-bf00-241681624586",
-    "paymentLinkUrl": "https://woovi.com/pay/9134e286-6f71-427a-bf00-241681624586",
-    "qrCodeImage": "https://api.woovi.com/openpix/charge/brcode/image/9134e286-6f71-427a-bf00-241681624586.png",
-    "expiresIn": 2592000,
-    "expiresDate": "2021-04-01T17:28:51.882Z",
-    "createdAt": "2021-03-02T17:28:51.882Z",
-    "updatedAt": "2021-03-02T17:28:51.882Z",
-    "brCode": "000201010212261060014br.gov.bcb.pix2584https://api.woovi.com/openpix/testing?transactionID=867ba5173c734202ac659721306b38c952040000530398654040.015802BR5909LOCALHOST6009Sao Paulo62360532867ba5173c734202ac659721306b38c963044BCA",
-    "additionalInfo": [
-      {
-        "key": "Product",
-        "value": "Pencil"
-      },
-      {
-        "key": "Invoice",
-        "value": "18476"
-      },
-      {
-        "key": "Order",
-        "value": "302"
-      }
-    ],
-    "paymentMethods": {
-      "pix": {
-        "method": "PIX_COB",
-        "transactionID": "9134e286-6f71-427a-bf00-241681624586",
-        "identifier": "9134e286-6f71-427a-bf00-241681624586",
-        "additionalInfo": [],
-        "fee": 50,
-        "value": 200,
-        "status": "ACTIVE",
-        "txId": "9134e286-6f71-427a-bf00-241681624586",
-        "brCode": "000201010212261060014br.gov.bcb.pix2584https://api.woovi.com/openpix/testing?transactionID=867ba5173c734202ac659721306b38c952040000530398654040.015802BR5909LOCALHOST6009Sao Paulo62360532867ba5173c734202ac659721306b38c963044BCA",
-        "qrCodeImage": "https://api.woovi.com/openpix/charge/brcode/image/9134e286-6f71-427a-bf00-241681624586.png"
-      }
-    }
-  }
-}
-JSON
+input =
+  if ARGV[0]
+    File.read(ARGV[0])
+  else
+    $stdin.read
+  end
 
-parsed = JSON.parse(json)
+atom_keys = ARGV.include?("--atoms")
 
-puts to_elixir(parsed, atom_keys: false)
+parsed = JSON.parse(input)
+puts to_elixir(parsed, atom_keys: atom_keys)
